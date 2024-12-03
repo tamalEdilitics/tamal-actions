58c426197fe536268b237d35be08acb20a378885
tamalEdilitics <tamal@edilitics.com>
2024-12-03 13:20:46 +0530

    fix:test6

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
-  const thisShouldGiveErrorLol = "This should give an error";
+  const wtfGiveErrorBro = "This should give an error";
 
   return (
     <>
